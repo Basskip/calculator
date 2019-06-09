a604998129afd1c9264df1a67d1243649f63da6a
Cameron Scott <camscottgreen@hotmail.com>
2019-06-09 11:54:16 +1000

    Calculator works to evaluate expressions

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-function Chunk(type, value) {
-    this.type = type;
-    this.value = value;
-}
-
 var expression = [];
 const screen = document.querySelector('#screen');
+var currentNumber = "";
 
 function buttonSetup() {
     const buttons = document.querySelectorAll('button');
@@ -18,10 +14,55 @@ function buttonSetup() {
 
 function pushButton(button) {
     console.log(button);
-    appendDisplay(button);
-    if(button === "Clear") {
+    
+    if (button === "Clear") {
         setDisplay('');
+        expression = [];
+        currentNumber = "";
+    } else if (button === "=") {
+        expression.push(parseInt(currentNumber));
+        setDisplay(evaluate(expression));
+        currentNumber = expression[0].toString();
+    } else if (/[0-9]/.test(button)) {
+        //A number was pressed
+        currentNumber += button;
+        appendDisplay(button);
+    } else {
+        expression.push(parseInt(currentNumber));
+        currentNumber = "";
+        expression.push(button);
+        appendDisplay(button);
     }
+}
+
+function evaluate(expr) {
+    while (expr.length > 1) {
+        var index = -1;
+        var evaluated = NaN;
+
+        index = expr.indexOf("x");
+        if (index === -1) {
+            index = expr.indexOf("÷");
+        }
+        if (index === -1) {
+            index = expr.indexOf("+");
+        }
+        if (index === -1) {
+            index = expr.indexOf("-");
+        }
+
+        evaluated = operate(expr[index - 1], expr[index + 1], expr[index]);
+        expr.splice(index - 1, 3, evaluated);
+    }
+    return expr[0];
+}
+
+function isNumber(text) {
+    return /[0-9]/.test(text);
+}
+
+function isOperator(text) {
+    return /[-+x÷]/.test(text);
 }
 
 function appendDisplay(text) {
@@ -45,7 +86,7 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    if(num2 === 0) {
+    if (num2 === 0) {
         return "Nice try!";
     }
     return num1 / num2;
@@ -57,9 +98,9 @@ function operate(num1, num2, operator) {
             return add(num1, num2);
         case "-":
             return substract(num1, num2);
-        case "*":
+        case "x":
             return multiply(num1, num2);
-        case "/": 
+        case "÷":
             return divide(num1, num2);
         default:
             return 0;
